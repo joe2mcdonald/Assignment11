7ce7b36d55dd1126a93dd6ad2c80749d989a09ab
joe2mcdonald <ns7@san.rr.com>
2015-12-15 16:23:39 -0800

    Assignment 11 update part 2

--- EmployeeDirectory/js/main.js
+++ EmployeeDirectory/js/main.js
@@ -10,15 +10,15 @@
 
 //  WEB WORKER API  -  SESSION STORAGE
 // create a web worker to retrieve JSON data in the background
-	var worker = new Worker("getJSON.js");							 // CREATES WORKER FILE
-		  console.log(" main 1A worker = " +   worker );	
-	worker.postMessage("data/employee.json");						 // PASSES THE URL OF THE JSON FILE
-	worker.onmessage = function(e) {                   				 // STORES THE DATA
-       // store the JSON data in session storage for later use
-	   sessionStorage.employeesSstor = JSON.stringify(e.data);   //CONVERT DATA TO STRING AND PUTS INTO SESSION STORAGE
+//	var worker = new Worker("getJSON.js");							 // CREATES WORKER FILE
+//		  console.log(" main 1A worker = " +   worker );	
+//	worker.postMessage("data/employees2.json");						 // PASSES THE URL OF THE JSON FILE
+//	worker.onmessage = function(e) {                   				 // STORES THE DATA
+//       // store the JSON data in session storage for later use
+//	   sessionStorage.employeesSstor = JSON.stringify(e.data);   //CONVERT DATA TO STRING AND PUTS INTO SESSION STORAGE
 	
-	  console.log(" main 1B sessionStorage.employeesSstor = " +   sessionStorage.employeesSstor );	
-	};
+//	  console.log(" main 1B sessionStorage.employeesSstor = " +   sessionStorage.employeesSstor );	
+//	};
 					
 					
 console.log("main 1C  ");
@@ -28,23 +28,58 @@ console.log("main 1C  ");
 	var subCnt = 0;								// subbordinates counter
 	
 	 // get JSON data from the employees.json file in the foreground
-	$.getJSON("data/employee.json", function(data) {
-		$.each(data, function(employees) {
-			var html = "";	
-			$.each(this, function(key, value) {   
+	$.getJSON("data/employees2.json", function(data) {
+		
+		$.each(data, function(employees) { 
+		var html = "";
+		$.each(this, function(key, value) { 			
+	//		$.each(this, function(i, name) {   
 					console.log ("main 2A " );
-		        html += '<li><a href=index.html#' + value.employeeId + ">"  + "<img src=" + value.imagepath + ">";
-				html += '<h3>' + value.name + '</h3>'; 
-			   	html += '<p>' + value.title + '<span class="ui-li-count">' + subCnt + '</span></p></a></li>'; 
-			console.log("  MAIN 2B   value.employeeId  = " + value.employeeId );
-			});
+		 html += '<li><a href=index.html#' + value.employeeId + ">"  + "<img src=" + value.imagepath + ">";
+		 html += '<h3>' + value.name + '</h3>'; 
+		 html += '<p>' + value.title + '<span class="ui-li-count">' + subCnt + '</span></p></a></li>';
+				
+	//			$("#employee").append(
+	//		li +=	'<li><a href="#" + value.employeeId + ">" + '</li>';
+	//		li += '<li><a href="#" id="' + i + '" class="info-go">' + name.name + '</a></li>';
+//				"<img src=" + value.imagepath + ">" +
+//				'<h3>' + value.name + '</h3>' +
+//					'<p>' + 
+//				value.title + '<span class="ui-li-count">' + subCnt + '</span>
+//				  </p></a></li>';
+//				);
+
+			});	
+				
+//			console.log("  MAIN 2B   value.employeeId  = " + value.employeeId );
 			
+/*	
+    //append list to ul
+    $("#prof-list").append(html).promise().done(function () {
+        //wait for append to finish - thats why you use a promise()
+        //done() will run after append is done
+        //add the click event for the redirection to happen to #details-page
+        $(this).on("click", ".info-go", function (e) {
+            e.preventDefault();
+            //store the information in the next page's data
+            $("#pageId2").data("info", info[this.id]);
+            //change the page # to second page. 
+            //Now the URL in the address bar will read index.html#details-page
+            //where #details-page is the "id" of the second page
+            //we're gonna redirect to that now using changePage() method
+            $.mobile.changePage("#pageId2");
+        });
+			
+    //refresh list to enhance its styling.
+        $(this).listview("refresh");	
+		
+*/		
 					console.log("main 3");
-			$("#empId").html(html); 
+		$("#empId").html(html); 
 		//	$("#empId").listview("refresh");
 			$("#empId").listview().listview('refresh');
 		});
-		
+//	});	
 	});      // end of getJSON
 
 
@@ -98,7 +133,7 @@ console.log("main 1C  ");
 	
 	
 	
- 		// });    		//  END pagecreate
+ 	//	 });    		//  END pagecreate
   });   // End document.addEventListener("DOMContentLoaded)	
 	
  
